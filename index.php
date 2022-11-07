f459e44a9b6a621fd27fc8af9b4e454c7a433876
Kirill Bystrov <kir9aza@gmail.com>
2022-11-07 15:25:00 +0300

    parameter existence check

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?
+<?php 
+$login = !empty($_GET['login'])?$_GET['login']:'Логин не передан';
+$password = !empty($_GET['password'])?$_GET['password']: 'Пароль не передан';
 
- ?>
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>GET REQUEST</title>
+</head>
+<body>
+	<p>Переданный логин: </p>
+	<?= $login ?>
+	<br>
+	<p>Переданный пароль: </p>
+	<?= $password?> 
+</body>
+</html>
