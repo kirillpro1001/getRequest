cf2077aaed742be6246fe6b739557cc435d3e63f
Kirill Bystrov <kir9aza@gmail.com>
2022-11-07 15:30:57 +0300

    creating a git request form

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
-<?php 
-$login = !empty($_GET['login'])?$_GET['login']:'Логин не передан';
-$password = !empty($_GET['password'])?$_GET['password']: 'Пароль не передан';
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>GET REQUEST</title>
+	<title>form git request</title>
 </head>
 <body>
-	<p>Переданный логин: </p>
-	<?= $login ?>
-	<br>
-	<p>Переданный пароль: </p>
-	<?= $password?> 
+	<form action="login.php" method = "get">
+		<label for="login">Логин:</login>
+			<input type="text" name="login"> 
+		<label for="password">Пароль:</login>
+			<input type="password" name="password"> 
+		<input type="submit">
+	</form>
 </body>
 </html>
